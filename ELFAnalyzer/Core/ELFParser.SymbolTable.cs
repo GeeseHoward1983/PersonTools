4403b30d5bab35653d2775464d02d6d0e5ba726c
阿捏利 <19168013@qq.com>
2026-01-05 09:15:50 +0800

    修复提示

--- ELFAnalyzer/Core/ELFParser.SymbolTable.cs
+++ ELFAnalyzer/Core/ELFParser.SymbolTable.cs
@@ -74,8 +74,8 @@ namespace PersonalTools.ELFAnalyzer.Core
         }
 
         // 添加字段记录符号表关联的字符串表索引
-        private Dictionary<SectionType, uint> _linkedStrTabIdx32 = [];
-        private Dictionary<SectionType, uint> _linkedStrTabIdx64 = [];        // 添加版本符号信息存储
+        private readonly Dictionary<SectionType, uint> _linkedStrTabIdx32 = [];
+        private readonly Dictionary<SectionType, uint> _linkedStrTabIdx64 = [];
         private ushort[]? _versionSymbols32;
         private ushort[]? _versionSymbols64;
         private Dictionary<ushort, string>? _versionDefinitions;
